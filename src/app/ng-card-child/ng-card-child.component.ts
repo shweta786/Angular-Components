cd811dcf591cbc39e77352adb27955de8a29c437
shweta786 <shwetasrivastava1994@gmail.com>
2017-12-07 16:31:55 +0530

    edit and commit

--- src/app/ng-card-child/ng-card-child.component.ts
+++ src/app/ng-card-child/ng-card-child.component.ts
@@ -11,10 +11,14 @@ export class NgCardChildComponent implements OnInit {
   ngOnInit() {
   }
 
+  private _isActive: boolean = false;
+  isDisabled: boolean = true;
+
   @Input() title: string;
   @Input() hidden: boolean = false;
   @Input() showNext: boolean = true;
   @Input() showPrev: boolean = true;
+  
   @Input('isActive')
   set isActive(isActive: boolean) {
     this._isActive = isActive;
@@ -22,9 +26,6 @@ export class NgCardChildComponent implements OnInit {
   }
   @Output() onNext: EventEmitter<any> = new EventEmitter<any>();
   @Output() onPrev: EventEmitter<any> = new EventEmitter<any>();
-
-  private _isActive: boolean = false;
-  isDisabled: boolean = true;
 
   get isActive(): boolean {
     return this._isActive;
--- src/app/ng-multiple-card/ng-multiple-card.component.ts
+++ src/app/ng-multiple-card/ng-multiple-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ContentChildren, QueryList, AfterContentInit } from '@angular/core';
+import { Component, OnInit, ContentChildren, QueryList, AfterContentInit, Output, EventEmitter } from '@angular/core';
 import { NgCardChildComponent } from '../ng-card-child/ng-card-child.component';
 @Component({
   selector: 'app-ng-multiple-card',
@@ -11,13 +11,62 @@ export class NgMultipleCardComponent implements AfterContentInit {
   ngOnInit() {
   }
 
+  @Output()
+  onStepChanged: EventEmitter<NgCardChildComponent> = new EventEmitter<NgCardChildComponent>();
+
   @ContentChildren(NgCardChildComponent) cardSteps: QueryList<NgCardChildComponent>;
   private _steps: Array<NgCardChildComponent>= [];
-  private steps: Array<NgCardChildComponent>= [];
 
   ngAfterContentInit() {
     this.cardSteps.forEach(step => this._steps.push(step));
     this.steps[0].isActive = true;
+  }
+
+  //getter method of _steps array, filtering step which is not hidden
+  get steps(): Array<NgCardChildComponent> {              
+    return this._steps.filter(step => !step.hidden);
+  }
+
+  get activeStep(): NgCardChildComponent {
+    return this.steps.find(step => step.isActive);
+  }
+
+  set activeStep(step: NgCardChildComponent) {
+    if (step !== this.activeStep && !step.isDisabled) {
+      this.activeStep.isActive = false;
+      step.isActive = true;
+      this.onStepChanged.emit(step);
+    }
+  }
+
+  public get activeStepIndex(): number {
+    return this.steps.indexOf(this.activeStep);
+  }
+
+  get hasNextStep(): boolean {
+    return this.activeStepIndex < this.steps.length - 1;
+  }
+
+  get hasPrevStep(): boolean {
+    return this.activeStepIndex > 0;
+  }
+
+  public next(): void {
+    if (this.hasNextStep) {
+      let nextStep: NgCardChildComponent = this.steps[this.activeStepIndex + 1];
+      this.activeStep.onNext.emit();
+      nextStep.isDisabled = false;
+      this.activeStep = nextStep;
+    }
+  }
+
+  public previous(): void {
+    if (this.hasPrevStep) {
+      let prevStep: NgCardChildComponent = this.steps[this.activeStepIndex - 1];
+      this.activeStep.onPrev.emit();
+      prevStep.isDisabled = false;
+      this.activeStep = prevStep;
+    }
   }
 
 }
